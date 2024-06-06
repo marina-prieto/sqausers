6ddb8e1a148f75d601f63ed4209768996f5132c2
Marina Prieto <marina.prieto1@alu.uclm.es>
2024-06-06 19:34:17 +0200

    SQL injection protection, parametrized searches

--- sqausers/src/main/java/dissw24/sqausers/dao/UserDAO.java
+++ sqausers/src/main/java/dissw24/sqausers/dao/UserDAO.java
@@ -3,10 +3,13 @@ package dissw24.sqausers.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dissw24.sqausers.model.User;
 
 public interface UserDAO extends JpaRepository<User, String> {
     
-	Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM users u WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
