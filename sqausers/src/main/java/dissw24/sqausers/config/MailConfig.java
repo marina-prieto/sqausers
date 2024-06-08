2794920c8df3598d486eb83c4f6b086d6b1ce4f7
Marina Prieto <marina.prieto1@alu.uclm.es>
2024-06-08 21:22:27 +0200

    mailconfig definition to fix issues

---
+++ sqausers/src/main/java/dissw24/sqausers/config/MailConfig.java
@@ -0,0 +1,31 @@
+// MailConfig.java
+package dissw24.sqausers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp-mail.outlook.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("hamiltonia.net@outlook.es");
+        mailSender.setPassword("HamilSw24");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
